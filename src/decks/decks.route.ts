5e301f411766d570602fa543f41711a0047c38e1
ThFoxY <davidmel7562@gmail.com>
2026-02-04 21:10:09 +0100

    feat(api): add Decks /api/decks/:id (PATCH) route to update Deck by ID
    Implement new API Decks route to update a Deck by its ID using PATCH.
    This allow authentificated Deck owner (JWT middleware) to update one of their Deck by ID.

--- src/decks/decks.route.ts
+++ src/decks/decks.route.ts
@@ -14,6 +14,7 @@ export const decksRouter = Router();
 decksRouter.post("/", authenticateToken, async (req: Request, res: Response) =>
 {
     // Destructuration : extrait les champs name et cards de la requête POST pour en faire des variables distinctes
+    // TODO : Créer un type pour req.body
     const {name, cards} = req.body;
 
     // Créer le deck contenant 10 cartes aléatoires exactement
@@ -85,7 +86,9 @@ decksRouter.get("/mine", authenticateToken, async (req: Request, res: Response) 
 // JWT : S'assure que le token est valide
 decksRouter.get("/:id", authenticateToken, async (req: Request, res: Response) =>
 {
+    // Récupérer l'ID en paramètre
     const deckId = Number(req.params.id);
+
     // Récupérer le Deck par son ID
     try {
         // 1. Vérifier si le deck existe
@@ -113,12 +116,99 @@ decksRouter.get("/:id", authenticateToken, async (req: Request, res: Response) =
 
         if (!deckById) {
             return res.status(403).json({error: "Deck inaccesible"});
-        }
+        };
 
         // 3. Retourner le Deck
         return res.status(200).json(deckById);
     } catch (error) {
         console.error("Error when getting deck by ID:", error);
+        return res.status(500).json({error: "Server error"});
+    };
+});
+
+// PATCH /api/decks/:id
+// Accessible via PATCH /api/decks/:id
+// JWT : S'assure que le token est valide
+decksRouter.patch("/:id", authenticateToken, async (req: Request, res: Response) =>
+{
+    // Récupérer l'ID en paramètre
+    const deckId = Number(req.params.id);
+
+    // Destructuration : extrait les champs name et cards de la requête PATCH pour en faire des variables distinctes
+    // TODO : Créer un type pour req.body
+    const {name, cards} = req.body;
+
+    // Récupérer le Deck par son ID
+    try {
+        // 0. Vérifier que tous les champs sont remplis (notamment s'assurer qu'il y a bien 10 cartes)
+        if (!name || !cards || cards.length !== 10) {
+            return res.status(400).json({error: "Données manquantes"});
+        };
+
+        // 1. Vérifier que toutes les IDs de cartes sont valides/existants
+        const existingCards = await prisma.card.findMany({
+            where: {
+                id: {
+                    in: cards,
+                },
+            },
+        });
+
+        // Vérifier la taille puisque findMany renvoie un tableau
+        if (existingCards.length !== 10) {
+            return res.status(400).json({error: "IDs de cartes Pokémon invalides/inexistants"});
+        };
+
+        // 2. Vérifier si le deck existe
+        const existingDeck = await prisma.deck.findUnique({
+            where: {
+                id: deckId,
+            }
+        });
+
+        if (!existingDeck) {
+            return res.status(404).json({error: "Deck introuvable"});
+        };
+
+        // 3. Vérifier si le deck appartient à l'utilisateur authentifié
+        const deckById = await prisma.deck.findUnique({
+            where: {
+                id: deckId,
+                userId: req.user!.userId,
+            }
+        });
+
+        if (!deckById) {
+            return res.status(403).json({error: "Deck inaccesible"});
+        };
+
+        // 4. Mettre à jour le Deck
+        const updatedDeck = await prisma.deck.update({
+            where: {
+                id: deckId,
+            },
+            data: {
+                name,
+                cards: {
+                    // Suppression des cartes existantes
+                    deleteMany: {},
+                    // Ajout des nouvelles cartes
+                    createMany: {
+                        // Map() permet d'étaler les 10 cartes en 10 objets distincts auxquels on associe un ID
+                        data: cards.map((cardId: number) => ({cardId})),
+                    },
+                },
+            },
+            // Inclure les cartes du Deck (Prisma s'occupe de lier la jointure DeckCard)
+            include: {
+                cards: true,
+            }
+        })
+
+        // 5. Retourner le Deck mis à jour
+        return res.status(200).json(updatedDeck);
+    } catch (error) {
+        console.error("Error when updating deck by ID:", error);
         return res.status(500).json({error: "Server error"});
     };
 });
