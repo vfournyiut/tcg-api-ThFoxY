298efa617d67b589d54a216ab11360ac1314681e
ThFoxY <davidmel7562@gmail.com>
2026-02-15 17:50:12 +0100

    ci(commitlint): update commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-max-length': [2, 'always', 100], // En-tête plus long
+    'body-max-line-length': [2, 'always', Infinity], // Corps plus long
+    'footer-max-line-length': [2, 'always', Infinity], // Pied de page plus long
     'type-enum': [
       2,
       'always',
