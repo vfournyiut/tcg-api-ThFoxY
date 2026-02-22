d18f247669f644667b9c867be0b1075864b606c4
ThFoxY <davidmel7562@gmail.com>
2026-02-22 21:29:42 +0100

    feat(socket): add username to `userData` and `connectedUsers` map

--- src/sockets/SocketServer.ts
+++ src/sockets/SocketServer.ts
@@ -8,12 +8,14 @@ import { Server, Socket } from 'socket.io'
  *
  * @property {function} user - Événement envoyé lorsqu'un utilisateur se connecte, avec le nom d'utilisateur en paramètre.
  * @property {function} getRooms - Événement envoyé pour demander la liste des salles disponibles.
+ * @property {function} createRoom - Événement envoyé pour créer une nouvelle salle.
  * @property {function} joinRoom - Événement envoyé lorsqu'un utilisateur rejoint une salle, avec le nom de la salle en paramètre.
  * @property {function} leaveRoom - Événement envoyé lorsqu'un utilisateur quitte une salle, avec le nom de la salle en paramètre.
  */
 interface ClientToServerEvents {
   user: (username: string) => void
   getRooms: () => void
+  createRoom: (room: string) => void
   joinRoom: (room: string) => void
   leaveRoom: (room: string) => void
 }
@@ -25,6 +27,7 @@ interface ClientToServerEvents {
  * @property {function} welcome - Événement envoyé pour accueillir un nouveau client, avec un message de bienvenue en paramètre.
  * @property {function} userJoined - Événement envoyé à tous les clients sauf celui qui vient de se connecter, avec un message indiquant qu'un nouvel utilisateur s'est connecté.
  * @property {function} roomsListUpdated - Événement envoyé au client demandeur avec la liste des salles en attente d'un second joueur. Chaque entrée contient l'identifiant de la salle et l'email du host.
+ * @property {function} roomCreated - Événement envoyé au client qui a créé une salle, avec les données de la salle (nom, email du host et liste des utilisateurs) en paramètre.
  * @property {function} roomJoined - Événement envoyé lorsqu'un utilisateur rejoint une salle, avec les données de la salle (nom et liste des utilisateurs) en paramètre.
  * @property {function} roomUserJoined - Événement envoyé à tous les clients d'une salle lorsqu'un nouvel utilisateur rejoint la salle, avec le nom de l'utilisateur en paramètre.
  * @property {function} roomUserLeft - Événement envoyé à tous les clients d'une salle lorsqu'un utilisateur quitte la salle, avec le nom de l'utilisateur en paramètre.
@@ -34,6 +37,11 @@ interface ServerToClientEvents {
   welcome: (message: string) => void
   userJoined: (message: string) => void
   roomsListUpdated: (rooms: { room: string; host: { email: string } }[]) => void
+  roomCreated: (data: {
+    room: string
+    host: { email: string }
+    users: string[]
+  }) => void
   roomJoined: (data: { room: string; users: string[] }) => void
   roomUserJoined: (username: string) => void
   roomUserLeft: (username: string) => void
@@ -45,10 +53,12 @@ interface ServerToClientEvents {
  * @interface UserData
  *
  * @property {number} userId - L'identifiant unique de l'utilisateur.
- * @property {string} username - Le nom d'utilisateur.
+ * @property {string} username - Le nom d'utilisateur (optionnel).
+ * @property {string} email - L'adresse email de l'utilisateur.
  */
 interface UserData {
   userId: number
+  username?: string
   email: string
 }
 
@@ -63,6 +73,7 @@ type TypedServer = Server<ClientToServerEvents, ServerToClientEvents>
 export class SocketServer {
   private io: TypedServer // Instance de Socket.io avec les types personnalisés
   private rooms: Map<string, Set<string>> // Map pour stocker les salles et les utilisateurs présents dans chaque salle
+  private connectedUsers: Map<number, string> // Mpa pour stocker les IDs des utilisateurs connectés et leur socket ID
 
   /**
    * @description Constructeur de la classe SocketServer. Initialise Socket.io et configure les événements de connexion.
@@ -80,6 +91,7 @@ export class SocketServer {
     )
 
     this.rooms = new Map<string, Set<string>>() // Initialiser la map des salles
+    this.connectedUsers = new Map<number, string>() // Initialiser la map des utilisateurs connectés
 
     this.setupAuthMiddleware() // Configure le middleware d'authentification pour Socket.io
     this.initializeSocket() // Configure les événements de connexion Socket.io
@@ -106,6 +118,16 @@ export class SocketServer {
           process.env.JWT_SECRET as string,
         ) as UserData
 
+        // 4. Vérifier si l'utilisateur est déjà connecté
+        if (this.connectedUsers.has(decoded.userId)) {
+          const existingSocketId = this.connectedUsers.get(decoded.userId)!
+          // Déconnecter la session précédente
+          this.io.sockets.sockets.get(existingSocketId)?.disconnect(true)
+          console.log(
+            `Utilisateur ${decoded.email} déjà connecté, déconnexion de la session précédente.`,
+          )
+        }
+
         // Stocker les données utilisateur dans le socket pour une utilisation ultérieure
         socket.data = decoded
         next()
@@ -126,6 +148,7 @@ export class SocketServer {
   private initializeSocket(): void {
     this.io.on('connection', (socket) => {
       const userData = socket.data as UserData // Récupérer les données utilisateur stockées dans le socket
+      this.connectedUsers.set(userData.userId, socket.id) // Ajouter l'utilisateur à la map des utilisateurs connectés
       console.log('Nouvelle connexion :', socket.id, `(${userData.email})`)
 
       // Envoyer un événement uniquement à ce client
@@ -134,6 +157,7 @@ export class SocketServer {
       // Gérer les événements envoyés par le client
       socket.on('user', () => this.handleUser(socket, userData))
       socket.on('getRooms', () => this.handleGetRooms(socket))
+      socket.on('createRoom', (room) => this.handleCreateRoom(socket, room))
       socket.on('joinRoom', (room) =>
         this.handleJoinRoom(socket, userData, room),
       )
@@ -166,7 +190,10 @@ export class SocketServer {
    * @param {TypedSocket} socket - Le socket du client qui a envoyé l'événement.
    */
   private handleGetRooms(socket: TypedSocket): void {
-    const availableRooms: { room: string; host: { email: string } }[] = [] // Liste pour stocker les salles disponibles avec l'email du host
+    const availableRooms: {
+      room: string
+      host: { email: string; username?: string }
+    }[] = [] // Liste pour stocker les salles disponibles avec l'email et le nom d'utilisateur du host
 
     // Parcourir les salles et filtrer celles qui ont exactement un joueur (le host)
     this.rooms.forEach((roomSet, room) => {
@@ -177,7 +204,10 @@ export class SocketServer {
         // Si le socket du host existe, extraire les données utilisateur pour obtenir l'email et ajouter la salle à la liste des salles disponibles
         if (hostSocket) {
           const hostData = hostSocket.data as UserData
-          availableRooms.push({ room, host: { email: hostData.email } })
+          availableRooms.push({
+            room,
+            host: { email: hostData.email, username: hostData.username },
+          })
         }
       }
     })
@@ -186,8 +216,41 @@ export class SocketServer {
   }
 
   /**
-   * @description Méthode pour gérer l'événement 'join-room' envoyé par le client lorsqu'un utilisateur veut rejoindre une salle.
-   * Ajoute l'utilisateur à la salle et envoie un événement 'user-joined-room' aux autres clients dans la salle.
+   * @description Méthode pour gérer l'événement 'createRoom' envoyé par le client lorsqu'un utilisateur veut créer une nouvelle salle.
+   * Créer une nouvelle salle et envoie un événement 'roomCreated' au créateur (host), ainsi qu'un evénement 'roomsListUpdated' aux autres clients.
+   * @private
+   *
+   * @param {TypedSocket} socket - Le socket du client qui a envoyé l'événement.
+   * @param {string} room - Le nom de la salle que l'utilisateur veut créer.
+   * @return {boolean | void} - Retourne false si la salle existe deja, sinon retourne void.
+   */
+  private handleCreateRoom(socket: TypedSocket, room: string): boolean | void {
+    // Vérifier si la salle existe deja
+    if (this.rooms.has(room)) {
+      return socket.emit('error', 'La salle existe déjà')
+    }
+
+    // Créer la salle et ajouter le créateur (host) à la salle
+    this.rooms.set(room, new Set<string>([socket.id]))
+    socket.join(room)
+
+    const userData = socket.data as UserData // Récupérer les données utilisateur pour obtenir l'email du host
+    socket.emit('roomCreated', {
+      room,
+      host: { email: userData.email },
+      users: [userData.email],
+    }) // Envoyer les données de la salle au client qui vient de la créer
+
+    socket.broadcast.emit('roomsListUpdated', [
+      { room, host: { email: userData.email } },
+    ]) // Informer les autres clients qu'une nouvelle salle a été créée
+
+    console.log(`${userData.email} a créé la salle ${room}`)
+  }
+
+  /**
+   * @description Méthode pour gérer l'événement 'joinRoom' envoyé par le client lorsqu'un utilisateur veut rejoindre une salle.
+   * Ajoute l'utilisateur à la salle et envoie un événement 'userJoinedRoom' aux autres clients dans la salle.
    * @private
    *
    * @param {TypedSocket} socket - Le socket du client qui a envoyé l'événement.
@@ -217,8 +280,8 @@ export class SocketServer {
   }
 
   /**
-   * @description Méthode pour gérer l'événement 'leave-room' envoyé par le client lorsqu'un utilisateur veut quitter une salle.
-   * Retire l'utilisateur de la salle et envoie un événement 'user-left-room' aux autres clients dans la salle.
+   * @description Méthode pour gérer l'événement 'leaveRoom' envoyé par le client lorsqu'un utilisateur veut quitter une salle.
+   * Retire l'utilisateur de la salle et envoie un événement 'userLeftRoom' aux autres clients dans la salle.
    * @private
    *
    * @param {TypedSocket} socket - Le socket du client qui a envoyé l'événement.
@@ -276,6 +339,10 @@ export class SocketServer {
    * @param {UserData} userData - Les données utilisateur extraites du token JWT.
    */
   private handleDisconnect(socket: TypedSocket, userData: UserData): void {
+    // Retirer l'utilisateur du map des utilisateurs connectés
+    this.connectedUsers.delete(userData.userId)
+
+    // Parcourir les salles et retirer l'utilisateur de toutes les salles auxquelles il appartient
     this.rooms.forEach((roomSet, roomName) => {
       if (roomSet.has(socket.id)) {
         roomSet.delete(socket.id) // Retirer le socket ID de la liste des utilisateurs de la salle
